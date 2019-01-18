c693656ad7f8e72391e46381c28ba0e09515fec8
GrahamNick <nick.g.coac@gmail.com>
2019-01-18 12:09:18 -0500

    Initial commit

---
+++ Space-Shooter-3-master/Space Shooter/Assets/bossFight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossFight : MonoBehaviour {
+	public float BossWaitTime;
+	public float nextAnimTime;
+	public float bossHealth;
+	public float moveBy;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (BossFight ());
+	}
+	IEnumerator BossFight() {
+		yield return new WaitForSeconds (BossWaitTime);
+		while (transform.position.z < 24f) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + moveBy);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
